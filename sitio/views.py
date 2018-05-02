b6f8ef9285140646ef94bd3d90e60d5b36dc8f12
luciano <lhitaliani@gmail.com>
2018-05-02 19:54:59 -0300

    la vista home tiene la logica correspondiente

--- sitio/views.py
+++ sitio/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
 
-def home(request):
-    return render(request, 'home.html')
+from noticias.models import Noticia
+
+def home(request):    
+    n = Noticia.objects.all()
+    return render(request, 'home.html', {'noticia': n})
