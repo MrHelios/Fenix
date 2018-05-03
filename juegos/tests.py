4ed73bb77820819de2fe9f33fd0446069bc10976
luciano <lhitaliani@gmail.com>
2018-05-03 15:53:54 -0300

    se agrego test para la app juego1 para comprobar status=404 (FALLA)

--- juegos/tests.py
+++ juegos/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
 
-# Create your tests here.
+class JuegoPageTest(TestCase):
+
+    def test_juego1_page(self):
+        r = self.client.get('/g/juego1')
+        self.assertEqual(r.status_code, 404)
+
+    def test_juego1_page(self):
+        r = self.client.get('/g/juego1/download')
+        self.assertEqual(r.status_code, 404)
