ba355db5992b861755a891038c6b574329b1fcad
luciano <lhitaliani@gmail.com>
2018-05-03 13:31:59 -0300

    se agregaron test para las vista de noticias y el modelo

--- noticias/tests.py
+++ noticias/tests.py
@@ -1,3 +1,40 @@
 from django.test import TestCase
+from noticias.models import Noticia
 
-# Create your tests here.
+class NoticiaPageTest(TestCase):
+
+    def test_ultimas_noticias_templates(self):
+        r = self.client.get('/noticias')
+        self.assertEqual(r.status_code, 200)
+        self.assertTemplateUsed(r, 'ultimas_noticias.html')
+
+    def test_noticia_id_200(self):
+        n = Noticia.objects.create()
+        r = self.client.get('/noticias/%d/' % (n.id,))
+        self.assertEqual(r.status_code, 200)
+
+    def test_noticia_id_200(self):
+        r = self.client.get('/noticias/99999/')
+        self.assertEqual(r.status_code, 404)
+
+    def test_noticia_id_templates_200(self):
+        n = Noticia.objects.create()
+        r = self.client.get('/noticias/%d/' % (n.id,))
+        self.assertTemplateUsed(r, 'noticia_id.html')
+
+    '''
+    Agregar un template para error 404:
+    
+    def test_noticia_id_template_404(self):
+        r = self.client.get('/noticias/1/')
+        self.assertEqual(r.content, '')
+    '''
+
+class NoticiaModelTest(TestCase):
+
+    def test_crear_noticia(self):
+        n = Noticia()
+        self.assertEqual(n.titulo, '')
+        self.assertEqual(n.descripcion, '')
+        self.assertEqual(n.text, '')
+        self.assertNotEqual(n.fecha, '')
