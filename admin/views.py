89ad4b3e1b75fd013a32153fd9b179fa7b854daf
luciano <lhitaliani@gmail.com>
2018-05-03 11:54:18 -0300

    la vista admin_login autentifica los pedidos + se creo el template

--- admin/views.py
+++ admin/views.py
@@ -1,10 +1,19 @@
 import os
+from django.contrib.auth import authenticate
 from django.core.files.storage import FileSystemStorage
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 
 def login_admin(request):
-    return HttpResponse('login admin')
+    if request.method == 'POST':
+        u = request.POST['admin']
+        p = request.POST['password']
+        user = authenticate(request, username=u, password=p)
+        if user is not None and user.is_superuser:
+            return redirect('/admin/subir')
+        else:
+            return redirect('/admin/login')
+    return render(request, 'login_admin.html')
 
 def  admin_subir(request):
     if request.method == 'POST':
