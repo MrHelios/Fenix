2a1e76a25d64d050441076f71751345f908f3f8d
luciano <lhitaliani@gmail.com>
2018-05-02 23:49:47 -0300

    ahora se puede subir archivo a la pagina

--- admin/views.py
+++ admin/views.py
@@ -1,8 +1,15 @@
-#from django.shortcuts import render
+import os
+from django.core.files.storage import FileSystemStorage
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 
 def login_admin(request):
     return HttpResponse('login admin')
 
 def  admin_subir(request):
-    return HttpResponse('admin subir')
+    if request.method == 'POST':
+        f = request.FILES['file']
+        fs = FileSystemStorage()
+        archivo = fs.save(os.path.join('download/',f.name), f)
+        return redirect('/')
+    return render(request, 'subir.html')
