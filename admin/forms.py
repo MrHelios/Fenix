e68b3230d9a97ec05edc69d8ff9a518fa9a2603e
luciano <lhitaliani@gmail.com>
2018-05-04 17:26:06 -0300

    se puede crear noticia mediante la vista noticia/crear

---
+++ admin/forms.py
@@ -0,0 +1,5 @@
+def es_valido(t,d,c):
+    valido = True
+    if t is '' or d is '' or c is '':
+        valido = False
+    return valido
--- admin/views.py
+++ admin/views.py
@@ -4,6 +4,9 @@ from django.core.files.storage import FileSystemStorage
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 
+from noticias.models import Noticia
+from admin.forms import es_valido
+
 ADMIN_URL_SUBIR = '/admin/subir'
 ADMIN_URL_LOGIN = '/admin/login'
 ADMIN_URL_LOGOUT = '/admin/logout'
@@ -43,9 +46,16 @@ def admin_subir(request):
         return HttpResponse(status=404, content='La pagina que quiere acceder no existe')
 
 def admin_crear_noticia(request):
-    if request.method == 'POST':
-        t = request.POST['titulo']
-        d = request.POST['descripcion']
-        c = request.POST['contenido']
-        return HttpResponse("%s %s %s" % (t,d,c))
-    return render(request, 'noticia_crear.html')
+    if request.user.is_superuser:
+        if request.method == 'POST':
+            t = request.POST['titulo']
+            d = request.POST['descripcion']
+            c = request.POST['contenido']
+            if es_valido(t,d,c):
+                n = Noticia.objects.create(titulo=t, descripcion=d, text=c)
+                return redirect('/')
+            else:
+                return HttpResponse(status=404, content='Hubo un error')
+        return render(request, 'noticia_crear.html')
+    else:
+        return HttpResponse(status=404, content='La pagina que quiere acceder no existe')
